3735176c0ce76fd7907e40a5827f2d72e8cfd37a
Unknown <lai225joseph@gmail.com>
2019-02-17 09:40:28 +0800

    mongodb crud action

---
+++ MongodbPractise/Product.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongodbPractise
+{
+    public class Product
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
---
+++ MongodbPractise/ProductDao.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongodbPractise
+{
+    public class ProductDao
+    {
+        private MongoClient _mongoClient;
+        private IMongoDatabase _db;
+        private IMongoCollection<Product> _products;
+
+        public ProductDao()
+        {
+            _mongoClient = new MongoClient();
+            _db = _mongoClient.GetDatabase("test");
+            _products = _db.GetCollection<Product>("Products");
+        }
+
+        public async Task<List<Product>> QueryAll()
+        {
+            return await _products.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public void Insert(Product product)
+        {
+            _products.InsertOne(product);
+        }
+
+        public void Update(Product product)
+        {
+            var filter = Builders<Product>.Filter.Eq("_id", 10);
+            var update = Builders<Product>.Update.Set("_id", product.Id);
+            _products.UpdateOne(filter, update);
+        }
+
+        public long Delete(string id)
+        {
+            var filter = Builders<Product>.Filter.Eq("_id", id);
+            var result = _products.DeleteOne(filter);
+            return result.DeletedCount;
+        }
+    }
+}
